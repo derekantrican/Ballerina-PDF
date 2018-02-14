27cdebef42b37fab3ebd731928b937b739835920
derekantrican <derekantrican@gmail.com>
2018-02-14 14:44:39 -0800

    Added remove/rotate methods for all,even,odd pages #4

--- Ballerina PDF/PDFActions.cs
+++ Ballerina PDF/PDFActions.cs
@@ -62,6 +62,106 @@ namespace Ballerina_PDF
             }
         }
 
+        public static bool RemoveEvenPages(string filePath, bool throwErrors = false)
+        {
+            try
+            {
+                #region Setup
+                if (!File.Exists(filePath))
+                    throw new Exception("File not found");
+
+                PdfReader reader = new PdfReader(filePath);
+                PdfDocument document = new PdfDocument(reader);
+                string tempFilePath = filePath.Replace(".pdf", "-temp.pdf");
+                PdfWriter writer = new PdfWriter(tempFilePath);
+                PdfDocument tempDocument = new PdfDocument(writer);
+                File.SetAttributes(tempFilePath, File.GetAttributes(tempFilePath) | FileAttributes.Hidden);
+                #endregion Setup
+
+                #region Logic
+                //-----------I don't like this and want to do it "properly"/differently (if I can)--------
+                List<int> pageOrder = Enumerable.Range(1, document.GetNumberOfPages()).ToList();
+                foreach (int i in Enumerable.Range(1, document.GetNumberOfPages()))
+                {
+                    if (i % 2 == 0)
+                        pageOrder.Remove(i);
+                }
+
+                foreach (int i in pageOrder)
+                    tempDocument.AddPage(document.GetPage(i).CopyTo(tempDocument));
+                //----------------------------------------------------------------------------------------
+                #endregion Logic
+
+                #region Teardown
+                document.Close();
+                tempDocument.Close();
+
+                File.Delete(filePath);
+                File.Move(tempFilePath, filePath); //Renames the tempFilePath to the original filePath
+                File.SetAttributes(filePath, File.GetAttributes(filePath) & ~FileAttributes.Hidden); //"unhide" the file (remove the hidden attribute that was associated with the tempFilePath)
+                #endregion Logic
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (throwErrors)
+                    MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace);
+
+                return false;
+            }
+        }
+
+        public static bool RemoveOddPages(string filePath, bool throwErrors = false)
+        {
+            try
+            {
+                #region Setup
+                if (!File.Exists(filePath))
+                    throw new Exception("File not found");
+
+                PdfReader reader = new PdfReader(filePath);
+                PdfDocument document = new PdfDocument(reader);
+                string tempFilePath = filePath.Replace(".pdf", "-temp.pdf");
+                PdfWriter writer = new PdfWriter(tempFilePath);
+                PdfDocument tempDocument = new PdfDocument(writer);
+                File.SetAttributes(tempFilePath, File.GetAttributes(tempFilePath) | FileAttributes.Hidden);
+                #endregion Setup
+
+                #region Logic
+                //-----------I don't like this and want to do it "properly"/differently (if I can)--------
+                List<int> pageOrder = Enumerable.Range(1, document.GetNumberOfPages()).ToList();
+                foreach (int i in Enumerable.Range(1, document.GetNumberOfPages()))
+                {
+                    if (i % 2 != 0)
+                        pageOrder.Remove(i);
+                }
+
+                foreach (int i in pageOrder)
+                    tempDocument.AddPage(document.GetPage(i).CopyTo(tempDocument));
+                //----------------------------------------------------------------------------------------
+                #endregion Logic
+
+                #region Teardown
+                document.Close();
+                tempDocument.Close();
+
+                File.Delete(filePath);
+                File.Move(tempFilePath, filePath); //Renames the tempFilePath to the original filePath
+                File.SetAttributes(filePath, File.GetAttributes(filePath) & ~FileAttributes.Hidden); //"unhide" the file (remove the hidden attribute that was associated with the tempFilePath)
+                #endregion Logic
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (throwErrors)
+                    MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace);
+
+                return false;
+            }
+        }
+
         public static bool ReorderPages(string filePath, List<int> newPageOrder, bool throwErrors = false)
         {
             try
@@ -136,6 +236,144 @@ namespace Ballerina_PDF
 
                 foreach (KeyValuePair<int, int> indexAnglePair in indiciesAndAngles)
                     tempDocument.GetPage(indexAnglePair.Key).SetRotation(document.GetPage(indexAnglePair.Key).GetRotation() + indexAnglePair.Value);
+                #endregion Logic
+
+                #region Teardown
+                document.Close();
+                tempDocument.Close();
+
+                File.Delete(filePath);
+                File.Move(tempFilePath, filePath); //Renames the tempFilePath to the original filePath
+                File.SetAttributes(filePath, File.GetAttributes(filePath) & ~FileAttributes.Hidden); //"unhide" the file (remove the hidden attribute that was associated with the tempFilePath)
+                #endregion Teardown
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (throwErrors)
+                    MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace);
+
+                return false;
+            }
+        }
+
+        public static bool RotateEvenPages(string filePath, int angle, bool throwErrors = false)
+        {
+            try
+            {
+                #region Setup
+                if (!File.Exists(filePath))
+                    throw new Exception("File not found");
+
+                PdfReader reader = new PdfReader(filePath);
+                PdfDocument document = new PdfDocument(reader);
+                string tempFilePath = filePath.Replace(".pdf", "-temp.pdf");
+                PdfWriter writer = new PdfWriter(tempFilePath);
+                PdfDocument tempDocument = new PdfDocument(writer);
+                File.SetAttributes(tempFilePath, File.GetAttributes(tempFilePath) | FileAttributes.Hidden);
+                #endregion Setup
+
+                #region Logic
+                for (int i = 1; i <= document.GetNumberOfPages(); i++)
+                    tempDocument.AddPage(document.GetPage(i).CopyTo(tempDocument));
+
+                for (int i = 1; i <= document.GetNumberOfPages(); i++)
+                {
+                    if (i % 2 == 0)
+                        tempDocument.GetPage(i).SetRotation(document.GetPage(i).GetRotation() + angle);
+                }
+                #endregion Logic
+
+                #region Teardown
+                document.Close();
+                tempDocument.Close();
+
+                File.Delete(filePath);
+                File.Move(tempFilePath, filePath); //Renames the tempFilePath to the original filePath
+                File.SetAttributes(filePath, File.GetAttributes(filePath) & ~FileAttributes.Hidden); //"unhide" the file (remove the hidden attribute that was associated with the tempFilePath)
+                #endregion Teardown
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (throwErrors)
+                    MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace);
+
+                return false;
+            }
+        }
+
+        public static bool RotateOddPages(string filePath, int angle, bool throwErrors = false)
+        {
+            try
+            {
+                #region Setup
+                if (!File.Exists(filePath))
+                    throw new Exception("File not found");
+
+                PdfReader reader = new PdfReader(filePath);
+                PdfDocument document = new PdfDocument(reader);
+                string tempFilePath = filePath.Replace(".pdf", "-temp.pdf");
+                PdfWriter writer = new PdfWriter(tempFilePath);
+                PdfDocument tempDocument = new PdfDocument(writer);
+                File.SetAttributes(tempFilePath, File.GetAttributes(tempFilePath) | FileAttributes.Hidden);
+                #endregion Setup
+
+                #region Logic
+                for (int i = 1; i <= document.GetNumberOfPages(); i++)
+                    tempDocument.AddPage(document.GetPage(i).CopyTo(tempDocument));
+
+                for (int i = 1; i <= document.GetNumberOfPages(); i++)
+                {
+                    if (i % 2 != 0)
+                        tempDocument.GetPage(i).SetRotation(document.GetPage(i).GetRotation() + angle);
+                }
+                #endregion Logic
+
+                #region Teardown
+                document.Close();
+                tempDocument.Close();
+
+                File.Delete(filePath);
+                File.Move(tempFilePath, filePath); //Renames the tempFilePath to the original filePath
+                File.SetAttributes(filePath, File.GetAttributes(filePath) & ~FileAttributes.Hidden); //"unhide" the file (remove the hidden attribute that was associated with the tempFilePath)
+                #endregion Teardown
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (throwErrors)
+                    MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace);
+
+                return false;
+            }
+        }
+
+        public static bool RotateAllPages(string filePath, int angle, bool throwErrors = false)
+        {
+            try
+            {
+                #region Setup
+                if (!File.Exists(filePath))
+                    throw new Exception("File not found");
+
+                PdfReader reader = new PdfReader(filePath);
+                PdfDocument document = new PdfDocument(reader);
+                string tempFilePath = filePath.Replace(".pdf", "-temp.pdf");
+                PdfWriter writer = new PdfWriter(tempFilePath);
+                PdfDocument tempDocument = new PdfDocument(writer);
+                File.SetAttributes(tempFilePath, File.GetAttributes(tempFilePath) | FileAttributes.Hidden);
+                #endregion Setup
+
+                #region Logic
+                for (int i = 1; i <= document.GetNumberOfPages(); i++)
+                    tempDocument.AddPage(document.GetPage(i).CopyTo(tempDocument));
+
+                for (int i = 1; i <= document.GetNumberOfPages(); i++)
+                    tempDocument.GetPage(i).SetRotation(document.GetPage(i).GetRotation() + angle);
                 #endregion Logic
 
                 #region Teardown
