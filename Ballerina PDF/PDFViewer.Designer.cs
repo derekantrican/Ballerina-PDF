0e69ad081d1b0f1a2311300530e6581fb4f983c4
derekantrican <derekantrican@gmail.com>
2018-02-19 12:16:40 -0800

    Added viewer form

---
+++ Ballerina PDF/PDFViewer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Ballerina_PDF
+{
+    partial class PDFViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PDFViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 664);
+            this.Name = "PDFViewer";
+            this.Text = "PDFViewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Ballerina PDF/PDFViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ballerina_PDF
+{
+    public partial class PDFViewer : Form
+    {
+        public PDFViewer(string filePath)
+        {
+            InitializeComponent();
+        }
+    }
+}
